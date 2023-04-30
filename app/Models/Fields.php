e687b7eb0e7c77e79cc59b540514a315a64ffb9f
asharinovaldy <asharinovaldi6319@gmail.com>
2023-04-30 10:22:38 +0800

    [AIRA-CORE] Create Database Migration (Fields Table)

---
+++ app/Models/Fields.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Fields extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = ['field_name', 'price_weekdays', 'price_weekends', 'price'];
+}
--- database/migrations/2023_04_30_013418_create_fields_table.php
+++ database/migrations/2023_04_30_013418_create_fields_table.php
@@ -15,7 +15,12 @@ class CreateFieldsTable extends Migration
     {
         Schema::create('fields', function (Blueprint $table) {
             $table->id();
+            $table->string('field_name');
+            $table->decimal('price_weekdays')->default(0)->nullable();
+            $table->decimal('price_weekends')->default(0)->nullable();
+            $table->decimal('price')->default(0)->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
