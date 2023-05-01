5e9b05a361f7fde08158014f5098897e09fd0d84
asharinovaldy <asharinovaldi6319@gmail.com>
2023-05-01 09:42:24 +0800

    [AIRA-CORE] Create Database Migration (Schedules Table)

---
+++ database/migrations/2023_04_30_034341_create_schedules_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('field_id');
+            $table->bigInteger('user_id');
+            $table->date('booking_date')->nullable();
+            $table->string('booking_time');
+            $table->enum('status', ['Pending', 'Transfer', 'Cash', 'Unpaid', 'Cancel', 'Done']);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
